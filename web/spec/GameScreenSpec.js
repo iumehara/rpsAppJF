096c6b8d85b79a54ba773dbdbf4057eb3a29bbc2
umehara <umehara@pro.local>
2018-04-26 23:34:20 +0900

    GameScreen displays result returned from Game on click

--- web/spec/GameScreenSpec.js
+++ web/spec/GameScreenSpec.js
@@ -1,10 +1,10 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
+const ReactTestUtils = require("react-dom/test-utils")
 const GameScreen = require('../src/GameScreen')
 
 describe("GameScreen", () => {
     var domFixture
-
     beforeEach(() => {
         domFixture = document.createElement("div")
         document.body.appendChild(domFixture)
@@ -20,6 +20,51 @@ describe("GameScreen", () => {
             domFixture
         )
 
+
         expect(domFixture.innerText).toContain('RPS App')
+    })
+
+    it('does not display results by default', () => {
+        ReactDOM.render(
+            <GameScreen/>,
+            domFixture
+        )
+
+        expect(domFixture.innerText).not.toContain('P1Wins')
+        expect(domFixture.innerText).not.toContain('P2Wins')
+        expect(domFixture.innerText).not.toContain('TIE')
+    })
+
+    it('displays result returned from Game', () => {
+        const gameStub = {
+            play: () => {
+                return 'P1Wins'
+            }
+        }
+
+        ReactDOM.render(
+            <GameScreen game={gameStub}/>,
+            domFixture
+        )
+
+
+        document.querySelector('button').click()
+
+
+        expect(domFixture.innerText).toContain('P1Wins')
+    })
+
+    it('displays no result if there is no game', () => {
+        const undefinedGameStub = undefined
+
+        ReactDOM.render(
+            <GameScreen game={undefinedGameStub}/>,
+            domFixture
+        )
+
+        document.querySelector('button').click()
+
+
+        expect(domFixture).toBeTruthy()
     })
 })
--- web/src/GameScreen.js
+++ web/src/GameScreen.js
@@ -1,14 +1,25 @@
 const React = require('react')
 
 class GameScreen extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    playButtonWasClicked() {
+        if (this.props.game === undefined) { return }
+        const result = this.props.game.play(this.state.p1Throw, this.state.p2Throw)
+        this.setState({result: result})
+    }
+
     render() {
         return (
             <div>
-                <h1>Wrong Title</h1>
+                <h1>RPS App</h1>
                 <label>Player 1<input name="p1Throw"/></label><br/>
                 <label>Player 2<input name="p2Throw"/></label><br/>
-                <button>play</button>
-                <div>Result:</div>
+                <button onClick={this.playButtonWasClicked.bind(this)}>play</button>
+                <div>Result: {this.state.result} </div>
             </div>
         )
     }
